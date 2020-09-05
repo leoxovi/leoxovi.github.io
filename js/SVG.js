e2e16a2987fc8b76840768158e23dcb431676f6d
leoxovi <leoxovi@hotmail.com>
2020-09-05 23:07:27 +0800

    updated about

---
+++ js/SVG.js
@@ -0,0 +1,12 @@
+$("#doughnutChart").drawDoughnutChart([
+  {
+    title: "Nope, It's all just the web",
+    value : 4822,
+    color: "#f3e32b"
+  },
+  {
+    title: "Yep. They are different things with different concerns",
+    value:  12339,
+    color: "#35a8ff"
+  }
+]);
